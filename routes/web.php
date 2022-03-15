517b404eb7134f81fe7ec8f5052c8dfaa1e8857c
Chris OC <christopher.oconnor@santismail.ch>
2022-03-15 14:31:50 +0100

    Del unused defaults

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
