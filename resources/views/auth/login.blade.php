6977299cc00d4f0e83886f189c25adebec0f8179
Chris OC <christopher.oconnor@santismail.ch>
2022-03-17 16:00:16 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 <div class="row justify-content-center">
     <div class="col-md-12 col-lg-8">
         <div class="card">
+            <div class="card-header text-center"><h4>Login</h4></div>
             <div class="card-body">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
