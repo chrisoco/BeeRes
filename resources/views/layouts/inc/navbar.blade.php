12cf378582093001f926a3e6e9704f9b66a3bb22
Chris OC <christopher.oconnor@santismail.ch>
2022-03-18 11:08:05 +0100

    Update navbar.blade.php

--- resources/views/layouts/inc/navbar.blade.php
+++ resources/views/layouts/inc/navbar.blade.php
@@ -8,7 +8,7 @@
         <div class="navbar-collapse collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="">INDEX</a>
+                    <a class="nav-link" aria-current="page" href="{{ route('index') }}">INDEX</a>
                 </li>
             </ul>
 
