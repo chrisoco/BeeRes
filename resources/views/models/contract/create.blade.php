b7c351e58a7dc55965944af4bdc886daa646fda4
Chris <christopher.oconnor@santismail.ch>
2022-03-21 10:29:38 +0100

    Frontend Contract Create

--- resources/views/models/contract/create.blade.php
+++ resources/views/models/contract/create.blade.php
@@ -13,16 +13,15 @@
                 @csrf
                 @method('POST')
 
-                <!-- Number? -->
-                <x-forms.input type="text"  name="lon" title="Longitude" value="{{ old('lon') }}" />
-                <x-forms.input type="text"  name="lat" title="Latitude" value="{{ old('lat') }}" />
+                <x-forms.number name="lat" title="Latitude"  min="47" max="48" placeholder="47.39036" value="{{ old('lat') }}" />
+                <x-forms.number name="lon" title="Longitude" min="8" max="9" placeholder="8.49087" value="{{ old('lon') }}" />
+
                 <hr />
                 <h4 class="text-center">Contact Information</h4>
                 <x-forms.input type="text"  name="contact_firstname" title="Firstname" value="{{ old('contact_firstname') }}" required="false" />
                 <x-forms.input type="text"  name="contact_lastname" title="Lastname" value="{{ old('contact_lastname') }}" required="false" />
                 <x-forms.input type="text"  name="contact_phone" title="Phone" value="{{ old('contact_phone') }}" required="false" />
 
-                <!-- Textarea -->
                 <x-forms.textarea name="info" title="Additional Information" value="{{ old('info') }}" required="false" />
 
 
