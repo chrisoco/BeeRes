585fa64d79bfe1166a6dd52dae0570df5304a21a
Chris <christopher.oconnor@santismail.ch>
2022-03-21 10:29:25 +0100

    Number Component

---
+++ app/View/Components/Forms/Number.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\View\Components\Forms;
+
+use Illuminate\View\Component;
+
+class Number extends Component
+{
+
+    /**
+     * The alert message.
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * The alert message.
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * The alert message.
+     *
+     * @var string
+     */
+    public $value;
+
+    /**
+     * The alert message.
+     *
+     * @var string
+     */
+    public $min;
+
+    /**
+     * The alert message.
+     *
+     * @var string
+     */
+    public $max;
+
+    /**
+     * The alert message.
+     *
+     * @var string
+     */
+    public $placeholder;
+
+    /**
+     * The alert message.
+     *
+     * @var boolean
+     */
+    public $required;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($name, $title, $value, $min, $max, $placeholder, $required = 'true')
+    {
+        $this->name  = $name;
+        $this->title = $title;
+        $this->value = $value;
+        $this->min   = $min;
+        $this->max   = $max;
+        $this->placeholder = $placeholder;
+        $this->required    = $required == 'true';
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.forms.number');
+    }
+}
---
+++ resources/views/components/forms/number.blade.php
@@ -0,0 +1,9 @@
+<div class="form-group">
+    <label class="form-label mt-4" for="{{ $name }}">{{ $title }} @if($required) <span class="text-danger">*</span> @endif</label>
+    <input class="form-control @if($errors->any()) @error($name) is-invalid @else is-valid @enderror @endif"
+           type="number" name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}" value="{{ $value }}"
+           min="{{ $min }}" max="{{ $max }}" step=".00001">
+    @error($name)
+    <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+</div>
